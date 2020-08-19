b6c166327752acdf7456850ecc8847138d206ef6
Reyzor <testaccwow001@gmail.com>
2020-08-20 02:05:50 +0300

    Updates

---
+++ precache-manifest.5ac4328814238897e92a278b6373d24e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaa9266754ffd82871d4415e52526b6",
+    "url": "/react-samurai/index.html"
+  },
+  {
+    "revision": "cfab0ca73f469d71cf26",
+    "url": "/react-samurai/static/css/3.a86cdc69.chunk.css"
+  },
+  {
+    "revision": "d85cf254435a10639783",
+    "url": "/react-samurai/static/css/4.34227b0f.chunk.css"
+  },
+  {
+    "revision": "4e867e1fc65ef984e4c8",
+    "url": "/react-samurai/static/css/main.542c7d58.chunk.css"
+  },
+  {
+    "revision": "8baa570e79f922771b9d",
+    "url": "/react-samurai/static/js/2.5a61cc9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-samurai/static/js/2.5a61cc9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfab0ca73f469d71cf26",
+    "url": "/react-samurai/static/js/3.6307ba66.chunk.js"
+  },
+  {
+    "revision": "d85cf254435a10639783",
+    "url": "/react-samurai/static/js/4.acb5d88c.chunk.js"
+  },
+  {
+    "revision": "4e867e1fc65ef984e4c8",
+    "url": "/react-samurai/static/js/main.cb0466ed.chunk.js"
+  },
+  {
+    "revision": "2ea45ad99031ee71f9a5",
+    "url": "/react-samurai/static/js/runtime-main.3bec730b.js"
+  },
+  {
+    "revision": "44bdc77bb04f85e4bdd68bdb9ab264a9",
+    "url": "/react-samurai/static/media/header.44bdc77b.jpg"
+  },
+  {
+    "revision": "24f97216ef7f7f3aa50009cf58574502",
+    "url": "/react-samurai/static/media/preloader.24f97216.gif"
+  },
+  {
+    "revision": "b3e90dfeb6b3b2de52eddd2c94ab19ab",
+    "url": "/react-samurai/static/media/user.b3e90dfe.png"
+  }
+]);
